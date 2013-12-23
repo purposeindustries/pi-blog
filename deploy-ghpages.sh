ee871ded96193fbf823e9ccfe3e5c9596d7d987d
Dányi Bence <madbence@gmail.com>
2013-12-23 11:09:17 +0100

    Deploy script added

---
+++ deploy-ghpages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf out
+node_modules/docpad/bin/docpad generate
+( cd out
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "dev@purposeindustries.co"
+  git add .
+  git commit -m "Deployed to Github Pages"
+  git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+)
