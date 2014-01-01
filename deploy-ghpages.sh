78597c45083ed7860122c9264db8fc4306717bf0
Dányi Bence <madbence@gmail.com>
2014-01-01 18:41:42 +0100

    Fix custom domain

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,6 +5,7 @@ node_modules/docpad/bin/docpad --silent generate
   git init
   git config user.name "Travis-CI"
   git config user.email "dev@purposeindustries.co"
+  cp ../CNAME ./CNAME
   git add .
   git commit -m "Deployed to Github Pages"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages >/dev/null
